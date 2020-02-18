8257583cd37302f969b8dbf87f66b231f16b97b9
Prasaanth <prasaanthselvakumar@gmail.com>
2020-02-18 17:59:30 +0530

    2000_nodes

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const app = express();
 const port = 3450;
 app.use(cors());
 const data = require('./miserables.json');
+const data1 = require('./miserables_1.json');
 const test_data=require('./test_json.json');
 app.get('/', (req, res) => res.send('The server is up and running!!!'));
 app.listen(port, () => console.log(`App listening in port ${port}!`));
 app.get('/data', function (req, res) {
   res.json(data);
+})
+app.get('/data1', function (req, res) {
+  res.json(data1);
 })
 app.get('/test', function (req, res) {
   res.json(test_data);
